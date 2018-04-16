0fe1cda0c8ba0ee3d5ab58c39492612ab9a4be67
nikola2444 <nikola2444@yahoo.com>
2018-04-16 14:10:54 +0200

    deskewing and writing to file done

--- number_recognition_example.py
+++ number_recognition_example.py
@@ -1,45 +1,102 @@
 """
 Created on Mon Apr  9 20:57:07 2018
 
-@author: Nikola
+@author: Kole
 """
-# to run this code you need to be in the folder where all the classes needed for this code to function are
+##############################################
+#LIBRARIES
+
 import numpy as np
 import matplotlib.pyplot as pt
 from tensorflow.examples.tutorials.mnist import input_data
 from SVM_number_recognition import svm_num_recognition
+import cv2
 
+##############################################
+# DESKEW FUNCTION
 
+def deskew(img_single):
+    img=np.reshape(img_single,(28,28))
+    SZ=28
+    m = cv2.moments(img)
+    if abs(m['mu02']) < 1e-2:
+        # no deskewing needed. 
+        return img.copy()
+    # Calculate skew based on central momemts. 
+    skew = m['mu11']/m['mu02']
+    # Calculate affine transform to correct skewness. 
+    M = np.float32([[1, skew, -0.5*SZ*skew], [0, 1, 0]])
+    # Apply affine transform
+    img = cv2.warpAffine(img, M, (SZ, SZ), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR)
+    return np.reshape(img,(1,784))
 
+##############################################
+# DESKEW DATASET
 
+def deskew_dataset(dataset):
+    ds=dataset.copy()
+    for i in range (0,np.shape(ds)[0]):
+        ds[i]=deskew(dataset[i])
+    return ds
+
+##############################################
+# IMPORTING DATASET
+    
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
 
+#############################################
+# TESTING 
+train_num = 100
+test_num = 100
+
+train_data = mnist.train.images[0:train_num]
+train_labels = mnist.train.labels[0:train_num] 
+test_data = mnist.test.images[0:test_num]
+test_labels = mnist.test.labels[0:test_num]
+
 sv = svm_num_recognition(kernel = 'poly', C = 1.6, degree = 3, sigma = 1, threshold = 1e-7)
-sv.svm_train(mnist.train.images[0:10000], mnist.train.labels[0:10000])
-a = sv.svm_validation(mnist.test.images[0:10000], mnist.test.labels[0:10000])
+sv.svm_train(train_data, train_labels)
+a = sv.svm_validation(test_data, test_labels)
 
-num=9980
-sv.svm_one_num_classification(mnist.test.images[num])
 
-#1101,1107,1116,1119,200,175
+sv = svm_num_recognition(kernel = 'poly', C = 1.6, degree = 3, sigma = 1, threshold = 1e-7)
+sv.svm_train(deskew_dataset(train_data),train_labels)
+a = sv.svm_validation(deskew_dataset(test_data), test_labels)
+
+
+
+#############################################
+# CLASSIFYING SINGLE IMAGE 
+
+num=1101
+sv.svm_one_num_classification(mnist.test.images[num]) #1101,1107,1116,1119,200,175
 d=np.reshape(mnist.test.images[num],(28,28)) 
 pt.imshow(d,cmap='Greys_r')
 
-# ovo sam pokusavao da testiram kako se menja procenat uspesnosti kada menjam C
+#############################################
+# TESTING DIFFERENT VALUES OF C
+
 percentage = np.zeros([100])
 j=0;
-for i in range(0,100):
+for i in range(0,30):
     j+=0.1
     sv = svm_num_recognition(kernel = 'poly', C = j, degree = 3, sigma = 1, threshold = 1e-7)
     sv.svm_train(mnist.train.images[0:200], mnist.train.labels[0:200])
-    percentage[i] = sv.svm_validation(mnist.test.images[0:1000], mnist.test.labels[0:1000])
+    percentage[i] = sv.svm_validation(mnist.test.images[0:1000], mnist.test.labels[0:1000])      
+percentage[np.argmax(percentage)]
 
-np.argmin(percentage)        
-percentage[41]
+#############################################
+# TESTING DESKEW FUNCTIONS
+    
+z=deskew(mnist.test.images[1101])
+zz=np.reshape(z,(28,28))
+pt.imshow(zz,cmap='Greys_r')
 
-##############################################
-X=np.array([[1,2,3],[4,5,6],[7,8,9]])
-z=np.array([0,0,0])
-for i in range(0,3):
-  z+=X[i]
-g=z.T+1
+#############################################
+# WRITING TO FILE
+z= np.array([[2,1,5,4,6],[1,2,3,5,4]])
+test = open("djole/test.txt",'w')
+np.savetxt(test,z,fmt='%.10f')
+test.close()
+
+
